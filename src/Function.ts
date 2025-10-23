48256d4b9176ee30dbe18fa3fae9dd5ab87b24a3
Joshua Amaju <joshsemail4work@gmail.com>
2025-10-23 22:45:32 +0100

    doc: add url helper example

--- src/Function.ts
+++ src/Function.ts
@@ -11,27 +11,28 @@ import { dual } from "./internal/utils.js";
  * @category combinator
  */
 export const andThen: {
-  <E1, A, B>(
-    fn: (res: A) => Either<E1, B>,
-  ): <E>(response: Either<E, A>) => Either<E | E1, B>;
+  <E1, A, B>(fn: (res: A) => Either<E1, B>): <E>(
+    response: Either<E, A>
+  ) => Either<E | E1, B>;
+
+  <E, A, E1, B>(response: Either<E, A>, fn: (res: A) => Either<E1, B>): Either<
+    E | E1,
+    B
+  >;
+
+  <E1, A, B>(fn: (res: A) => Promise<Either<E1, B>>): <E>(
+    response: Either<E, A>
+  ) => Promise<Either<E | E1, B>>;
 
   <E, A, E1, B>(
     response: Either<E, A>,
-    fn: (res: A) => Either<E1, B>,
-  ): Either<E | E1, B>;
-
-  <E1, A, B>(
-    fn: (res: A) => Promise<Either<E1, B>>,
-  ): <E>(response: Either<E, A>) => Promise<Either<E | E1, B>>;
-
-  <E, A, E1, B>(
-    response: Either<E, A>,
-    fn: (res: A) => Promise<Either<E1, B>>,
+    fn: (res: A) => Promise<Either<E1, B>>
   ): Promise<Either<E | E1, B>>;
 } = dual(2, (response, fn) => core.andThen(response, fn));
 
 /**
  * @since 0.0.1
  * @category combinator
+ * @example fetch(url("https://reqres.in/api/users/{id}", { id: 2 }))
  */
 export const url: (template: string, variables: object) => string = core.url;
